2cebf316e4ef52a1b35009da1841eedd719f869d
Henry Bubert <cryptix@riseup.net>
2009-10-30 10:55:22 +0100

    Rest modulo debug

--- uebung1/rest.c
+++ uebung1/rest.c
@@ -1,33 +1,1 @@
-/* Rechnet den Rest von Eingabe A und B ohne Modulo Operator
- *
- * Gruppe 42:
- * - Henry Bubert	   
- * - Jannik Thei√ü	 
- * - Jan Winkelmann
- */
-#include <stdio.h>
-#include <stdlib.h>
-
-
-int main(int argc, char *argv[]) {
-	int a, b;
-	
-	if(argc < 3) {
-		fprintf(stderr, "Usage: %s <a> <b>\n", argv[0]);
-		return -1;
-	}
-	
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	
-	if (a <= 0 || b <= 0) {
-		fprintf(stderr, "Bitte Werte ungleich 0 eingeben.\n");
-		return -2;
-	}
-	
-	
-	printf("Toll: %d %d\n",a, b);
-	
-	
-	return 0;
-}
+/* Rechnet den Rest von Eingabe A und B ohne Modulo Operator * * Gruppe 42: * - Henry Bubert	    * - Jannik Theiß	  * - Jan Winkelmann */#include <stdio.h>#include <stdlib.h>int modulo(int a, int b);int main(int argc, char *argv[]) {	int a, b;		if(argc < 3) {		fprintf(stderr, "Usage: %s <a> <b>\n", argv[0]);		return -1;	}		a = atoi(argv[1]);	b = atoi(argv[2]);		if (a <= 0 || b <= 0) {		fprintf(stderr, "Bitte Werte ungleich 0 eingeben.\n");		return -2;	}		printf("Eingabe: %d %d\n",a, b);			printf("Kontroll Rest:%d\n",a%b);		return 0;}int modulo(int a, int b) {	int s, m;		if (a*b < 0)		s = -1;				return s*m;}
