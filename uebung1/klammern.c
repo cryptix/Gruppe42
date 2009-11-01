893c2d70642c3aeba05297e236d2fa480d727a82
keks <keks@unstable.nl>
2009-11-01 16:32:31 +0100

    argh

--- uebung1/klammern.c
+++ uebung1/klammern.c
@@ -1,107 +1,128 @@
 /* Findet heraus, bei welchen a,b,c,d,e die Terme 'perms[]' unterschiedlich sind.
-*
-* von Gruppe 42:
-*   Jannik Theiß
-*   Henry Bubert
-*   Jan Winkelmann
-	a=b=c=d=e=-GRENZE; /*	a,b,c,d und e werden zu Beginn auf das negative 
-												Ebenbild der Grenze gesetzt */
-*
-*/
-
-#include<time.h> 	/*	Die normale time() ist immer geSegFaulted */
-#include<stdio.h>	/*	fuer printf. irgendwie logisch.						*/
-
-#define GRENZE 3								// Grenze als Konstante/Makro definieren
-#define ANZAHL_PERMUTATIONEN 5  // Anzahl der Klammerkombinationen definieren
-
+ *
+ * von Gruppe 42:
+ *   Jannik Theiß
+ *   Henry Bubert
+ *   Jan Winkelmann
+ *
+ */
 /*gibt perms, a, b, c, d und e huebsch aus. */
 void huebschAusgeben(int a,int b,int c,int d,int e); 
+
+#include <time.h> 	/* fuer notRandom() */
+#include <stdio.h>
+
+#define GRENZE 3   /* Grenze als Konstante/Makro definieren */
+#define PERMCNT 5  /* Anzahl der Klammerkombinationen definieren */
+
+void huebschAusgeben(int a,int b,int c,int d,int e, short int perms[]); 
+short int notRandom(short int max);  
+int noEquals(short int arr[]); 
+
+int main(void){
+	/* Deklariert a, b, c, d und e vom Typ short int,
+	 * werden fuer die Klammerkombinationen deklarieren 
+	 */
+	short int a,b,c,d,e;
+
+	/* Deklaration des short int Arrays 'perms', mit PERMCNT Elementen,
+	 * wird fuer die Ergebnisse der Terme verwendet.
+	 */
+	short int perms[PERMCNT];
 	
-int main(); /* Um dem Compiler von vornherein zu sagen, welche Funktionen es gibt. 
-						In diesem Fall sind das nicht so viele...*/
+	/* Deklariert und Definiert 'treffer' vom Typ short int,
+	 * wird fuer die Anzahl der unterschiedlichen Termwerte verwendet.
+	 * Am Anfang noch nie, also 0
+	 */
+	short int treffer = 0;
+	
+	/* Deklariert und Definiert 'rand' vom Typ short int,
+	 * wird verwendet um unterschiedliche Belegung 
+	 * fuer die Variablen a bis f zu liefern.
+	 */
+	short int rand = notRandom(750);
 
-/* Gibt eine mehr oder weniger zufaellige Zahl zurueck */
-long int notRandom(int max);  
+	/* Die Hauptschleife wird mit a,b,c,d und e = -GRENZE initialisiert,
+	 * laeuft solange a kleiner ist als die Grenze,
+	 * und in jedem Durchgang wird e inkrementiert.
+	 * Wenn e die Grenze erreicht, wird d inkrementiert.
+	 * Wenn d die Grenze erreicht, wird c inkrementiert. usw.
+	 */
+	for(a=b=c=d=e=-GRENZE; a<GRENZE; e++) {
+		if(e==GRENZE){ e=-GRENZE; d++; }	   
+		if(c==GRENZE){ c=-GRENZE; b++; }	   
+		if(d==GRENZE){ d=-GRENZE; c++; }      
+		if(b==GRENZE){ b=-GRENZE; a++; }      
 
-char sindAlleAnders(); /*	Funktion, die auf der Basis von a,b,c,d,e und 
-													perms herausfindet, ob alle Perms unterschiedlich 
-													sind.*/
+		/* Um zu verhindern, dass durch 0 geteilt wird */
+		if(d==e || d ==0){e++; continue;} 
 
-/*	Array der Permutationen global, weil andere Funktionen 
-		die auch noch brauchen. Ich weiss, haesslich. */
-short int perms[ANZAHL_PERMUTATIONEN]; 
-
-int main(){
-	short int treffer;		// Die Iteratoren fuer die Ueberpruefungsschleife definieren.
-	short int a,b,c,d,e;	// Die fuer die Klammerkombis gebrauchten Variablen definieren
-	long int rand = notRandom(500);
-	treffer=0;			// Zaehlt, wie oft alle Ergebnisse unterschiedlich waren. Am Anfang noch nie, also 0
-
-		
-	/*	Die Mainloop laeuft, solange a kleiner ist als die Grenze. In jedem Durchgang wird e 
-			inkrementiert. wenn e die Grenze erreicht. wird d inkrementiert, wenn d die Grenze 
-			erreicht, wird c inkrementiert, usw.	*/
-	for( a=b=c=d=e=-GRENZE; a<GRENZE; e++){		/*	a,b,c,d und e werden mit -GRENZE initialisiert,
-																								die Schleife wird weitergefuehrt, solange a<0 ist,
-																								und am ende der Schleife wird e inkrementiert.	*/
-		if(e==GRENZE){e=-GRENZE;d++;}	/* Wenn e die Grenze erreicht hat, d erhoehen und e reseten */
-		if(c==GRENZE){c=-GRENZE;b++;}	/* s.o. */
-		if(d==GRENZE){d=-GRENZE;c++;} /* s.o. */
-		if(b==GRENZE){b=-GRENZE;a++;} /* s.o. */
-		if(d==e || d ==0){e++; continue;} /* Um zu verhindern, dass durch 0 geteilt wird */
-
-		/* a bis e sind zugewiesen, jetzt wird in jedem durchgang der Wert der Terme ausgerechnet 
-		und im Array gespeichert */
+		/* a bis e sind zugewiesen,
+		 * jetzt wird in jedem durchgang der Wert der Terme ausgerechnet 
+		 * und im Array gespeichert
+		 */
 		perms[0] = a*b+c/d-e;
 		perms[1] = (a*b+c)/d-e;
 		perms[2] = (a*b+c)/(d-e);
 		perms[3] = (a*(b+c))/(d-e);
 		perms[4] = a*(b+c)/d-e;
-		
-		if(sindAlleAnders()>0){ // Falls sindAlleAnders() > 0 ist
-			if(rand == treffer){ //falls die Treffernummer "ausgelost" wurde
-				huebschAusgeben(a,b,c,d,e); /*Saubere Ausgabe der Permutationen und der Ergebnisse*/
-				return 0; //Das Programm nach der ersten Ausgabe beenden
-			}
-			treffer++; //Die Treffernummer inkrementieren. Muss ja jeder eine eigene haben.
+
+		/* Dieser Block ueberprueft ob die ergebnisse sich unterscheiden
+		 * und gibt eine mehr oder weniger zufaellige Kombination der Variablen aus
+		 */
+		if(noEquals(perms)==0 && rand == treffer++) {
+			/* Saubere Ausgabe der Permutationen und der Ergebnisse */
+			huebschAusgeben(a,b,c,d,e,perms); 
+
+			/* Das Programm nach der ersten Ausgabe beenden */
+			return 0;
 		}
 	}
+
 	return 0;
 }
 
-long int notRandom(int max){
-	int ret = (int) time(NULL)%4223; //eine mehr oder weniger zufaellige Zahl erstellen
-	while(ret > max){ //solange diese groesser ist als gewollt
-		//ret = ret/(max/8); //kleiner machen
-		ret = ret%(max);
-	}
-	return ret; //und dann zurueckgeben
+/* eine mehr oder weniger zufaellige Zahl ausgeben */
+short int notRandom(int max){
+	int ret = (int) time(NULL)%4223;            
+
+	while( (ret %= max) > max ); /* solange ret groesser ist als gewollt,  */
+	
+	return (short int) ret; /* und dann zurueckgeben */
 }
 
-char sindAlleAnders(){
+/* Untersucht ein Array auf gleiche Inhalte.
+ * Wenn zwei gleich sind gib 1 zureuck, sonst 0.
+ */
+int noEquals(short int arr[]){
 	int i,j;
-	char alleAnders=1; /* Wir gehen davon aus, dass bei allen Termen etwas anderes herauskommt */
-	for(i=0;i<ANZAHL_PERMUTATIONEN;i++){   // Zum Vergleichen gehen wir in zwei
-		for(j=0;j<ANZAHL_PERMUTATIONEN;j++){ // for-Schleifen, da wir zwei Werte vergleichen muessen.
-				if(i==j) continue;	// Damit wir nicht einen Term mit sich selbst vergleichen
-				if(perms[i] == perms[j]) {	//Falls dann doch mal einer gleich ist...
-					alleAnders=0; // Wird zuerst alleAnders auf 0 gesetzt.
 
-				}
+	/* Um festzustellen ob arr[] zwei gleiche Werte enthaehllt,
+	 * benutzen wir 2 verschachtelte for-Schleifen
+	 * und suchen nach gleichen Werten mit unterschiedlichen Indizes.
+	 */
+	for(i=0; i<PERMCNT; i++){        
+		for(j=0; j<PERMCNT; j++){
+			/* Damit wir nicht ein Element mit sich selbst vergleichen */
+			if(i==j) continue;
+
+			/* Falls zwei gleich sind... */
+			if(arr[i] == arr[j]) return 1;
 		}
 	}
-	return alleAnders;
-}
-void huebschAusgeben(int a,int b,int c,int d,int e){
-	//printf("Die moeglichen, mathematisch verschiedenen Kombinationen sind:\n\n");
-	printf("a=%d, b=%d, c=%d, d=%d, e=%d\n\n" ,a,b,c,d,e); //a-e werden mit ihren Werten ausgegeben
-	printf("a*b+c/d-e\t= %d\n", perms[0]);  //Alle Permutationen mit ihren Werten ausgeben
-	printf("(a*b+c)/d-e\t= %d\n", perms[1]);
-	printf("(a*b+c)/(d-e)\t= %d\n", perms[2]);
-	printf("(a*(b+c))/(d-e)\t= %d\n",perms[3]);
-	printf("a*(b+c)/d-e\t= %d\n",perms[4]);
-	//printf("Diese sind unterschiedlich, wenn a,b,c,d und e z.B. so gewaehlt werden:\n");
 
-	//printf("Sie sorgen naemlich fuer die oben angezeigten Ergebnisse\n");
+	return 0; /* keine gleichen gefunden */
+} 
+
+/* gibt a, b, c, d, e und die Terme huebsch aus. */
+void huebschAusgeben(int a, int b, int c, int d, int e, short int perms[]){
+	/* a bis e mit ihren Werten ausgegeben */
+	printf("a=%d, b=%d, c=%d, d=%d, e=%d\n\n", a, b, c, d, e); 
+
+	/* Termpermutationen mit ihren Werten ausgeben */
+	printf("a*b+c/d-e\t= %d\n",       perms[0]);      
+	printf("(a*b+c)/d-e\t= %d\n",     perms[1]);
+	printf("(a*b+c)/(d-e)\t= %d\n",   perms[2]);
+	printf("(a*(b+c))/(d-e)\t= %d\n", perms[3]);
+	printf("a*(b+c)/d-e\t= %d\n",     perms[4]);
 }
