2d44735534ff05c843ae4de6ba8e6eab68ef61b7
JTee <JTee@jtee-mbp-osx.fritz.box>
2009-11-13 21:18:43 +0100

    Userinput added

--- uebung3/zweierkomplement.c
+++ uebung3/zweierkomplement.c
@@ -7,13 +7,26 @@
  * - Jan Winkelmann
  *
 */
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-	short int a = -5, c = 0;
-	unsigned short int bitMask;
-	unsigned int b[16] = {0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,0};
-	int i;
+int main(int argc, char *argv[]) {
+	short int i, j, a = 42, c = 0;
+	unsigned short int bitMask, b[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, bIn[16] = {0};
+	
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s <Dec-Number> <Bin-Number>\nUsing Default Dec-Number = 42 and Bin-Number = 1\n", argv[0]);
+	} else {
+		a = atoi(argv[1]);
+		
+		for (i = 0; argv[2][i] != 0; i++) {
+			bIn[i] = (argv[2][i] == 48) ? 0 : 1;
+		}
+		
+		for (j = 0; j < i; j++) {
+			b[j] = bIn[i - j - 1];
+		}
+	}
 	
 	for (bitMask = 0x8000; bitMask > 0; bitMask >>= 1) {
 		printf("%d", (a & bitMask) != 0);
@@ -21,7 +34,7 @@ int main() {
 	
 	printf("\n");
 	
-	for (i = 0; i < 16; i++) {
+	for (i = 15; i >= 0; i--) {
 		c <<= 1;
 		c |= b[i];
 	}
