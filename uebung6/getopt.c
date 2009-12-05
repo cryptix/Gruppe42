8b04ef3cbba056a94e2bccf70db09ad5dc37c0cf
cryptix <cryptix@riseup.net>
2009-12-06 00:35:25 +0100

    getopt() cleanup

--- uebung6/getopt.c
+++ uebung6/getopt.c
@@ -8,35 +8,30 @@ static int getch(void);
 static void ungetch(int);
 
 int getopt(char *s) {
-	int i, c, next;
-	int opt;
-
+	int i, c, opt;
 	
 	while((s[0] = c = getch()) == ' ' || c == '\t'); /* skip whitespaces */
 	s[1]='\0'; /* just in case */
-	
+
+	opt = s[0] - '0';
+
 	/* no menu point */
-	opt = atoi(s);
-	if(opt < 1 || opt > 8)
-		return UNKWN;
-
-
-	while((c = getch()) == ' ' || c == '\t'); /* skip whitespaces */
-	ungetch(c); /* push back last char which isnt space or tab */
+	if(opt < 1 || opt > 8) return UNKWN;
 	
-	if(opt == 1 || opt == 4) {
-		i = 0;
-		
-		if(isdigit(c)) /* collect integer input */
-			while(isdigit(s[i++] = c = getch()));
+	switch(opt) {
+		case 1:
+		case 4:
+			while((c = getch()) == ' ' || c == '\t'); /* skip whitespaces */
+			ungetch(c);
 
-		s[i] = '\0';
-		
-		return opt;
-	} else
-		return opt; /* all other options dont have input */
-	
-	return UNKWN;
+			i=0;
+			if(isdigit(c))
+				while(isdigit(s[i++] = c = getch()));
+			s[i] = '\0';
+		default:
+			return opt;
+			break;
+	}
 }
 
 /* get a (possibly pushed-back) character */
