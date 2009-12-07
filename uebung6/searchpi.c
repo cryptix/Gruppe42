2e4791e949ee5c9d6c274e129e3ea8e37e6133b4
cryptix <cryptix@riseup.net>
2009-12-07 10:31:41 +0100

    searchpi.c

--- uebung6/searchpi.c
+++ uebung6/searchpi.c
@@ -1,23 +1,11 @@
 #include "pi.h"
 
-int strCmp(char* str1, char* str2, int* status);
-
 int searchpi(char *num) {
-	int status, i, foo;
-	for(i=0;status==1;i++)
-		strCmp(pi,num,&status);
-	return i;
-}
+	int i, ret=-1, numLen = strlen(num);
 
-int strCmp(char* str1, char* str2, int* status){
-	char* varstr1;
-	int i;
-	for(varstr1=str1;strlen(varstr1) >0; varstr1 ++)
-		if(strcmp(varstr1,str2) ==0){
-			*status=0;
-			return ( ( (int) str1) - ( (int) varstr1) );
-		}
-	*status = 1;
-	return -1;
-}
+	for(i=0;i < piLen; i++) {
+		if(strncmp(pi+i,num, numLen) == 0) ret = i;
+	}
 
+	return ret;
+}
