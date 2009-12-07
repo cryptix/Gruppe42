a40f7f2e50d7975c50120b5c69bcdfae4b00b1cd
keks <keks@unstable.nl>
2009-12-07 08:58:10 +0100

    naja, mal schaun

--- uebung6/searchpi.c
+++ uebung6/searchpi.c
@@ -1,11 +1,23 @@
 #include "pi.h"
 
+int strCmp(char* str1, char* str2, int* status);
+
 int searchpi(char *num) {
-	/* bogus */
-	if(strcmp(pi, num) == 0) {
-		return 42;
-	}
-	
+	int status, i, foo;
+	for(i=0;status==1;i++)
+		strCmp(pi,num,&status);
+	return i;
+}
+
+int strCmp(char* str1, char* str2, int* status){
+	char* varstr1;
+	int i;
+	for(varstr1=str1;strlen(varstr1) >0; varstr1 ++)
+		if(strcmp(varstr1,str2) ==0){
+			*status=0;
+			return ( ( (int) str1) - ( (int) varstr1) );
+		}
+	*status = 1;
 	return -1;
 }
 
