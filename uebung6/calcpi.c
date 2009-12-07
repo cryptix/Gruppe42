25744b6488a69544f3df827c3568105e2dd5f992
JTee <JTee@JTee-MBP-OSX.local>
2009-12-07 09:21:38 +0100

    calcpi lääääuft

--- uebung6/calcpi.c
+++ uebung6/calcpi.c
@@ -2,38 +2,44 @@
 
 /* calc pi to n decimal points */
 int calcpi(void) {
-	long int i, j;
-	long int *piRow;
-	long int rowLen;
+	int i, j;
+	int *piRow; /* Reihenglieder */
+	int rowLen, almostRowLen; /* Anzahl Reihenglieder */
+	int a;
 	
+	/* Arraylängen festlegen */
 	free(pi);
 	if( (pi = (char *) malloc(sizeof(char) * piLen)) == NULL) {
 		return -1;
 	}
 
-	rowLen = 10 * piLen / 3 +1; 
-	if( (piRow = (long int *) malloc(sizeof(long int) * rowLen)) == NULL) {
+	rowLen = 10 * piLen / 3 + 1;
+	if( (piRow = (int *) malloc(sizeof(int) * rowLen)) == NULL) {
 		return -1;
 	}
 
+	/* Alle Reihenglieder mit 2 initialisieren */
 	for (i = 0; i < rowLen; i++) {
 		piRow[i] = 2;
 	}
 
+	/* Berechnung der Dezimalstellen von pi */
+	almostRowLen = rowLen - 1;
 	for (i = 0; i < piLen; i++) {
 
-		j = rowLen - 1;
+		j = almostRowLen;
 		piRow[j] *= 10;
 		for (; j > 0; j--) {
-			piRow[j-1] *= 10;
-			piRow[j-1] += j * (piRow[j] / (2 * j + 1));
-			piRow[j] %= 2 * j + 1;
+			piRow[j-1] *= 10; /* Jedes Reihenglied mit neuer Basis 10 multiplizieren */
+			a = 2 * j + 1;
+			piRow[j-1] += j * (piRow[j] / a); /* Normaliesierung der Reihenglieder */
+			piRow[j] %= a;
 		}
 
-		pi[i] = piRow[0] / 10 + 48;
-		piRow[0] %= 10;
+		pi[i] = piRow[0] / 10 + 48; /* Dezimalstelle aus höchstwertigstem Reihenglied berechnen (und Umformung in ASCII-Wert) */
+		piRow[0] %= 10; /* Berechnete Dezimalstelle von der Reihe subtrahieren */
 
-		j = i;
+		j = i; /* Mögliche unaufgelöste Überträge auflösen */
 		while (pi[j] == 58) {
 			pi[j] = 48;
 			pi[j-1] += 1;
