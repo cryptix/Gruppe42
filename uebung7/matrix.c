5202d6f17464dee7c359855920c9c541891ef1b5
keks <keks@unstable.nl>
2009-12-08 12:31:30 +0100

    Jemand hat dich lieb <3

--- uebung7/matrix.c
+++ uebung7/matrix.c
@@ -1,1 +1,22 @@
 #include "matrix.h"
+
+matrix unterdet(matrix matr, int spalte);
+	
+
+int determ(matrix *matr){
+	
+}
+
+matrix unterdet(matrix matr, int spalte){
+	int i, j;
+	matrix ret;
+	
+	for(i=0; i<ordnung; i++){
+		
+		
+		
+	
+
+	}
+}
+
--- uebung7/matrix.h
+++ uebung7/matrix.h
@@ -3,16 +3,21 @@
 
 typedef struct{
 	int ordnung
+	int *wert;
+} vector;
+
+typedef struct{
+	int ordnung
 	int **wert;
 } matrix;
 
 enum { ORDER=1, INA, DETA, INVA, SOLVE, HELP, QUIT, UNKWN};
 
-int determ(matrix *matr); /* matrix.c */
-int defordnung(matrix *matr, int ordnung); /* matrix.c */
-int invert(matrix *matr, matrix *inv); /* matrix.c */
-int solve(matrix *matr); /* lgs.c */
-int wertegeben(matrix *matr); /* io.c */
+int determ(matrix *matr); /* matrix.c - jan */
+int defordnung(matrix *matr, int ordnung); /* matrix.c - jannik */
+int invert(matrix *matr, matrix *inv); /* matrix.c - henry */
+int solve(matrix *matr); /* lgs.c - jan */
+int wertegeben(matrix *matr); /* io.c - jannik*/
 
-int getopt(char *); /* getopt.c */
+int getopt(char *); /* getopt.c - henry */
 
