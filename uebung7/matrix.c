2b7dabb4e10b7af1a05ca5863ab8f1036857298a
keks <keks@unstable.nl>
2009-12-08 22:42:25 +0100

    subdeterminanten stehen wohl. to be tested.

--- uebung7/matrix.c
+++ uebung7/matrix.c
@@ -1,22 +1,32 @@
 #include "matrix.h"
 
-matrix unterdet(matrix matr, int spalte);
-	
+typedef struct{
+	int koeff;
+	matrix matr;
+} subDet;
+
+int getSubDet(matrix *matr, int reihe, int spalte, subDet* ret);
 
 int determ(matrix *matr){
-	
+	matrix* foo = allocMat(3,3);
+	foo->werte={1,2,3,4,5,6,7,8,10};
+	matrix* subdet = allocMat(2,2);
 }
 
-matrix unterdet(matrix matr, int spalte){
-	int i, j;
-	matrix ret;
-	
-	for(i=0; i<ordnung; i++){
-		
-		
-		
-	
+int getSubDet(matrix *matr, int reihe, int spalte, subDet* ret){
+	int i,I,j,J;
 
+	if(matr->col != matr->row) return -1;
+	if(ret->matr.col != ret->matr.row) return -2;
+	if(matr->col != ret->matr.row) return -3;
+
+	for(i=I=1; i<=matr->row; i++){
+		if(i==reihe) continue; 
+		for(j=J=1; j<=matr->col;j++){
+			if(j==spalte) continue;
+			ret->matr.wert[I*ret->matr.col + J] = matr->wert[i*matr->col + j];
+		}
 	}
-}
 
+	return 0;
+}
