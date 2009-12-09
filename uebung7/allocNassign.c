d8028789293b40051a42c2a1c956a8f639078703
cryptix <cryptix@riseup.net>
2009-12-09 20:39:22 +0100

    error is printed in allocMat()

--- uebung7/allocNassign.c
+++ uebung7/allocNassign.c
@@ -16,10 +16,7 @@ int main(void) {
 	int i, j;
 
 	mat = allocMat(12,12);
-	if(mat == NULL) {
-		fprintf(stderr, "allocation failed\n");
-		return -1;
-	}
+	if(mat == NULL) return -1;
 
 	for(i=0; i < mat->col; i++) {
 		for(j=0; j < mat->row; j++) {
