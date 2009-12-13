12613085d38d2239cd97f2401742f2fdcaca9bce
cryptix <cryptix@riseup.net>
2009-12-14 00:38:14 +0100

    invert() done -just replaces wert array with inverted one

--- uebung7/matrix.c
+++ uebung7/matrix.c
@@ -1,12 +1,13 @@
 #include "matrix.h"
 
-double getSubDet(double*, int);
+static double  getSubDet(double*, int);
+static matrix* getSubMat(matrix*, unsigned int, unsigned int);
 
 double getDet(matrix *mat) {
 	return getSubDet(mat->wert, mat->col);
 }
 
-double getSubDet(double *vals, int order) {
+static double getSubDet(double *vals, int order) {
 	int subOrder = order - 1;
 	double *subVals;
 	double subDet = 0;
@@ -33,6 +34,63 @@ double getSubDet(double *vals, int order) {
 	free(subVals);
 
 	return subDet;
+}
+
+/* Invertiert mat 'in place' */
+int invert(matrix *mat) {
+	unsigned int i, j;
+	float detA = 0;
+	matrix *tmp;
+	double *inv;
+
+	/* if matrix is invertible */
+	if(mat->row != mat->col) return -1;
+	if((detA = getDet(mat)) == 0) return -1;
+
+	/* allocate space for inverted matrix */
+	inv = (double *) malloc(sizeof(double) * mat->col * mat->col);
+	if(inv == NULL) return -1;
+
+	for(i=0; i < mat->row; i++) {
+		for(j=0; j < mat->col; j++) {
+			tmp = getSubMat(mat, j, i); /* strike i-col and j-row */
+			inv[mat->row * j + i] = ((i+j)%2 == 0 ? 1 : -1) * getDet(tmp)/detA;
+			freeMat(tmp);
+		}
+	}
+
+	free(mat->wert);
+	mat->wert=inv;
+
+	return 0;
+}
+
+/* Streicht Spalte col und Zeile row
+ * Gibt die resultierende Matrix zurueck
+ */
+static matrix *getSubMat(matrix *mat, unsigned int col, unsigned int row) {
+	unsigned int i, I, j, J; /* counters */
+    unsigned int order = mat->row - 1; /* mat is square */
+	matrix *tmp;
+
+	if (order == 0) return mat;
+
+	tmp = allocMat(order, order);
+	if(tmp == NULL) return NULL;
+
+
+	for(i=I=0; i < mat->row; i++) {
+		if(i == row) continue;
+		for(j=J=0; j < mat->col; j++) {
+			if(j != col) {
+				tmp->wert[tmp->row * I + J] = mat->wert[mat->row * i + j];
+				J++;
+			}
+		}
+		I++;
+	}
+
+	return tmp;
 }
 
 /* Einheitsmatrix erstellen */
