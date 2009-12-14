123d5a941d46b32c7fd93ac15b8402bbc80fbede
JTee <JTee@JTee-MBP-OSX.local>
2009-12-14 10:16:07 +0100

    io kommentiert

--- uebung7/io.c
+++ uebung7/io.c
@@ -2,21 +2,21 @@
 
 #define BUFSIZE 64
 
-int getNextParam(char*);
-int setOrder(matrix*);
-int setValues(matrix*);
-int setBValues(matrix*);
+int getNextParam(char*); /* Nächste Zeichenkette aus dem Eingabepuffer auslesen */
+int setOrder(matrix*); /* Rang der Matrix festlegen */
+int setValues(matrix*); /* Werte der Matrix festlegen */
+int setBValues(matrix*); /* Werte des Vektors b festlegen */
 
-int getopt(matrix *mat) {
+int getopt(matrix *mat) { /* Eingegebene Option auslesen */
 	char opt;
 
-	opt = getchar() - '0';
+	opt = getchar() - '0'; /* ASCII-Umwandlung */
 
 	if (opt == EOF) return EOF;
 
-	if (opt < 1 || opt > QUIT) return UNKWN;
+	if (opt < 1 || opt > QUIT) return UNKWN; /* Unbekannte Option */
 
-	switch(opt) {
+	switch(opt) { /* Eingegebene Option erwartet Parameter */
 		case 1:
 			if (!setOrder(mat)) return UNKWN;
 			break;
@@ -34,12 +34,12 @@ int getopt(matrix *mat) {
 int getNextParam(char *param) {
 	int i = 0;
 
-	while ((param[0] = getchar()) == ' ' || param[0] == '\t' || param[0] == '\n');
+	while ((param[0] = getchar()) == ' ' || param[0] == '\t' || param[0] == '\n'); /* Bis zum ersten Zeichen gehen */
 	i = 1;
 
-	while ((param[i] = getchar()) != ' ' && param[i] != '\t' && param[i] != '\n') {
+	while ((param[i] = getchar()) != ' ' && param[i] != '\t' && param[i] != '\n') { /* Zeichenkette bis zum nächsten Whitespace auslesen */
 		i++;
-		if(i >= BUFSIZE) return 0;
+		if(i >= BUFSIZE) return 0; /* Fehler: Buffer übeschritten */
 	}
 	param[i] = '\0';
 
@@ -50,12 +50,12 @@ int setOrder(matrix *mat) {
 	char param[BUFSIZE + 1];
 	int order;
 
-	if (!getNextParam(param)) return 0;
+	if (!getNextParam(param)) return 0; /* Parameter auslesen */
 
 	if ((order = atol(param)) < 1) return 0;
 
 	freeMat(mat);
-	mat = allocMat(order, order);
+	mat = allocMat(order, order); /* Neue Matrix mit eingegebenen Rang erstellen */
 
 	return 1;
 }
@@ -66,7 +66,7 @@ int setValues(matrix *mat) {
 	double value;
 
 	for (i = 0; i < (int)(mat->row * mat->col); i++) {
-		if (!getNextParam(param)) return 0;
+		if (!getNextParam(param)) return 0; /* Für jeden der n^2 Werte einen Parameter einlesen */
 
 		value = atof(param);
 
@@ -82,7 +82,7 @@ int setBValues(matrix *mat) {
 	double value;
 
 	for (i = 0; i < (int)(mat->col); i++) {
-		if (!getNextParam(param)) return 0;
+		if (!getNextParam(param)) return 0; /* Für jeden der n Werte einen Parameter einlesen */
 
 		value = atof(param);
 
