1594f7284cd24d6a642015aa9e744aa163203b96
cryptix <cryptix@riseup.net>
2010-01-12 13:28:25 +0100

    README updated

--- uebung9/stringconcatenate.c
+++ uebung9/stringconcatenate.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	int strLenSum = 0;
-	int i;
+	int strLenSum = 0, i;
 	char *concated;
+	FILE *fp;
 	
 	for(i=1; i<argc; i++)
 		strLenSum += strlen(argv[i]);
@@ -23,6 +23,14 @@ int main(int argc, char *argv[]) {
 	
 	printf("concated: %s\n", concated);
 	
+	fp = fopen("fname", "w");
+	if(fp == NULL) {
+		fprintf(stderr, "errrrrrr\n");
+		exit(1);
+	}
+
+	fprintf(fp, "%s\n", concated);
+	fclose(fp);
 	free(concated);
 
 	return 0;
